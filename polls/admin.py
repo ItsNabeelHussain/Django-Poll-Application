536c2cb8591d5d1c0a8e155f7c2a1ef483703f7a
Nabeel-Python-Developer <93263502+Nabeel-Python-Developer@users.noreply.github.com>
2021-11-03 14:49:30 +0500

    Update admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -3,11 +3,11 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
-
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
+    
 class QuestionAdmin(admin.ModelAdmin):
     # fieldsets = [
     #     (None,                      {'fields': ['question_text']}),
@@ -17,5 +17,6 @@ class QuestionAdmin(admin.ModelAdmin):
     search_fields = ['question_text']
     list_display = ('question_text', 'pub_date', 'was_published_recently')
     inlines = [ChoiceInline]
+    
 admin.site.register(Question, QuestionAdmin)
 
